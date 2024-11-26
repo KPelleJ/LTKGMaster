cd9788b8f1613fc585f863b5fc8af487fc00c6bc
Daniel Hartkopf <dani65h0@edu.zealand.dk>
2024-11-26 13:44:32 +0100

    SalesAd

---
+++ Folders/Models/SalesAd.cs
@@ -0,0 +1,18 @@
+﻿namespace LTKGMaster.Folders.Models
+{
+    public class SalesAd
+    {
+        public string Title { get; set; }
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateOfCreation { get; set; }
+
+    public SalesAd(string title, int productId, int userId, DateTime dateTimeOfCreation)
+        {
+            Title = title;
+            ProductId = productId;
+            UserId = userId;
+            DateOfCreation = dateTimeOfCreation;
+        }
+    }
+}
