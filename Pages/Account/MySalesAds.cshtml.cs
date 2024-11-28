26c78a0259b2edf963001282caa885d5ad1856ac
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-11-28 12:30:26 +0100

    lots of razorpages made with links to eachother

---
+++ Pages/Account/MySalesAds.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LTKGMaster.Pages.Account
+{
+    public class MySalesAdsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
