d4976a21eaff75fbfced5d9bb97378d6afb71f19
Daniel Hartkopf <dani65h0@edu.zealand.dk>
2024-11-28 12:13:33 +0100

    SalesAdRepo done, penis in my ass

--- Models/Repositories/SalesAdRepository.cs
+++ Models/Repositories/SalesAdRepository.cs
@@ -4,6 +4,7 @@ using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
 
 namespace LTKGMaster.Models.Repositories
 {
@@ -15,7 +16,7 @@ namespace LTKGMaster.Models.Repositories
         {
             _connectionString = configuration.GetConnectionString("myDb1");
         }
-        public void Add(SalesAd.SalesAd salesAd)
+        public void Add(ISalesAd salesAd)
         {
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
@@ -31,7 +32,7 @@ namespace LTKGMaster.Models.Repositories
             }
         }
 
-        public void Delete(SalesAd.SalesAd salesAd)
+        public void Delete(ISalesAd salesAd)
         {
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
@@ -46,7 +47,7 @@ namespace LTKGMaster.Models.Repositories
                 }
             }
         }
-        public void Update(SalesAd.SalesAd salesAd)
+        public void Update(ISalesAd salesAd)
         {
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
@@ -63,6 +64,36 @@ namespace LTKGMaster.Models.Repositories
                     command.ExecuteNonQuery();
                 }
             }
+        }
+        public List<SalesAd.SalesAd> GetAll(ISalesAd salesAd)
+        {
+            List<SalesAd.SalesAd> salesAds = new List<SalesAd.SalesAd>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT ProdID, UserId, Title, DataOfCreation FROM SalesAds";
+
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                command.Parameters.AddWithValue("@ProdId", salesAd.ProductId);
+
+                SqlDataReader reader = command.ExecuteReader();
+                {
+                    while (reader.Read())
+                    {
+                        SalesAd.SalesAd salesAd1 = new SalesAd.SalesAd
+                        {
+                            ProductId = reader.GetInt32(reader.GetOrdinal("ProdId")),
+                            UserId = reader.GetInt32(reader.GetOrdinal("UserId")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            DateOfCreation = reader.GetDateTime(reader.GetOrdinal("DateOfCreation"))
+                        };
+                        salesAds.Add(salesAd1);
+                    }
+                }
+            }
+            return salesAds;
         }
     }
 }
---
+++ Pages/SalesAd/UpdateSalesAd.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LTKGMaster.Pages.SalesAd
+{
+    public class UpdateSalesAdModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
