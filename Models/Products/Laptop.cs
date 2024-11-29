74afee96baf73b86d52a600a6d29cfa1632038b1
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-11-29 10:35:28 +0100

    aaa

--- Models/Products/Laptop.cs
+++ Models/Products/Laptop.cs
@@ -10,16 +10,16 @@ namespace LTKGMaster.Models.Products
         [RegularExpression(@"^[0-9a-zA-ZæøåÆØÅ._-]+$")]
         public string Storage {  get; set; }
         [Range(0, 150)]
-        [RegularExpression(@"^[0-9a-zA-ZæøåÆØÅ@._-]+$")]
+        [RegularExpression(@"^[0-9a-zA-ZæøåÆØÅ._-]+$")]
         public string OperateSystem { get; set; }
         [Range(0, 100)]
-        [RegularExpression("@\"^[0-9a-zA-ZæøåÆØÅ@._-]+$")]
+        [RegularExpression("@\"^[0-9a-zA-ZæøåÆØÅ._-]+$")]
         public string GPU { get; set; }
         [Range(0, 100)]
-        [RegularExpression("@\"^[0-9a-zA-ZæøåÆØÅ@._-]+$")]
+        [RegularExpression("@\"^[0-9a-zA-ZæøåÆØÅ._-]+$")]
         public string RAM { get; set; }
         [Range(0, 100)]
-        [RegularExpression("@\"^[0-9a-zA-ZæøåÆØÅ@._-]+$")]
+        [RegularExpression("@\"^[0-9a-zA-ZæøåÆØÅ._-]+$")]
         public string CPU { get; set; }
         
         public Laptop(string model, int year, string brand, double price, string description) : 
