3246feeb00723d47f01b11def6af571a3bd0a77a
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-12-06 12:56:58 +0100

    en smule gui-design ikke helt tilfreds endnu

--- Models/Products/StandardProduct.cs
+++ Models/Products/StandardProduct.cs
@@ -1,5 +1,8 @@
 ﻿namespace LTKGMaster.Models.Products
 {
+    /// <summary>
+    /// Dette er vores standard product subklasse som vi bruger når vi laver et nyt product.
+    /// </summary>
     public class StandardProduct:Product
     {
     }
