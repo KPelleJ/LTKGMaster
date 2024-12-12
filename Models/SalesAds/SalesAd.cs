5e9448633c8e9d0cb9ac661f6983ab8ddb2d60f4
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-12-12 10:03:10 +0100

    kan oprette igen og slette salgs annoncer

--- Models/SalesAds/SalesAd.cs
+++ Models/SalesAds/SalesAd.cs
@@ -13,10 +13,10 @@ namespace LTKGMaster.Models.SalesAds
         public int UserId { get; set; }
         [Required]
         public string Title { get; set; }
-        public Product Product { get; set; }
-        public IUser User { get; set; }
+        public Product ?Product { get; set; }
+        public IUser ?User { get; set; }
         public DateTime DateOfCreation { get; set; }
-        public List<ProductPicture> ProductPictures { get; set; }
+        public List<ProductPicture> ?ProductPictures { get; set; }
 
         public SalesAd()
         {
